0073cd35ea9a57358aec04de1a1487b4abe475b8
a7xsuri <leonardofilipposurico@gmail.com>
2022-06-14 21:53:31 +0200

    insert migrate

---
+++ database/migrations/2022_06_14_194615_create_journey_packet.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJourneyPacket extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journey_packet', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('city', 100);
+            $table->text('description');
+            $table->float('price', 8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journey_packet');
+    }
+}
